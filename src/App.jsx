db5611d792d7a0ae20867c59e9413421f9c311d3
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-12-31 13:20:04 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <nav className="navbar">
+      <div className="navdiv">
+        <div className="logo">
+          <a href="#">
+          Reabetswe Makgabutla
+          </a>
+        </div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Expertise</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Experience</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+     </nav>
+    </>
+  )
+}
+
+export default App
