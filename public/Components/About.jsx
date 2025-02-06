b4fb791189ebea94edcac67109cf061c0bd36826
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-02-06 11:32:15 +0200

    moved images to public folder

--- public/Components/About.jsx
+++ public/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="about-box">
         <div className="about-one">
         <img
-            src="/src/assets/profile-image.png"
+            src="/public/profile-image.png"
             alt="React Image"
             width="350"
             className="profile-img"
